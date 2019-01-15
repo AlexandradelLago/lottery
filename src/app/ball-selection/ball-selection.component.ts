6d7c638219bbfe318a77c6cfe6831d3726ce0245
Davinia Roca <davinia.roca.campos@elca.ch>
2019-01-15 17:43:40 +0100

    balls and colors

--- src/app/ball-selection/ball-selection.component.ts
+++ src/app/ball-selection/ball-selection.component.ts
@@ -8,9 +8,34 @@ import balls from '../utils/data';
 })
 export class BallSelectionComponent implements OnInit {
   balls = balls;
+  ballsColor:Array<String> = [];
   constructor() { }
 
   ngOnInit() {
+   this.ballsColor =  this.generateRandomColor(this.ballsColor);
+
+
   }
 
+ /* changeBackgroundColor() {
+    var colorBg =   document.getElementById("color-overlay")
+    colorBg.style.backgroundColor = this.generateRandomColor(this.ballsColor);
+  }*/
+
+  generateRandomColor(ballsColor) {
+    let i=0;
+    while (i<10){
+      ballsColor.push( `radial-gradient(circle at 10px 10px,#${Math.floor(Math.random()*16777215).toString(16)}, #000)`);
+      i++;
+    }
+    return ballsColor ;
+    }
+
+    setMyColor(i){
+      let color ={
+        'background': this.ballsColor[i]
+      }
+      return color ;
+  }
+  
 }
